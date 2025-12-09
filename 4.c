280ab76f86d164437c0e296765467b76016ac6a3
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2025-12-10 07:58:13 +0800

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",  &n);
+    if((n %10) == 0){
+        printf("Jebb"); 
+    }else{
+        printf("Neibb");
+    }
+return 0;    
+}
+// IA DID IT WITH NO PROBLEM
+// https://open.kattis.com/problems/storafmaeli
