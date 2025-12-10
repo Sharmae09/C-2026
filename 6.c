765171d8ef6b50bd4fbf0c8adbec3b2713cfd6bb
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2025-12-10 14:51:12 +0800

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    double m = n * 0.09144;
+    printf("%lf", m);
+    return 0;
+}
+
+// The problem with me is I keep forggetting to add pointer & 
