84c8893b9f590a8e58355eedf9b51e69c6d4877a
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2025-12-17 09:55:20 +0800

    Add program to subtract 0.3 from input number

---
+++ 16.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    double n;
+    scanf("%lf", &n);
+    printf("%.2lf", n - 0.3);
+    return 0;
+}
+
+// https://open.kattis.com/problems/abovesealevel?editresubmit=18868436
+// I solved without understanding the problem
