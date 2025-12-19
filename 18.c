7b653b1bca9d26346b7687429b39960cfeb89781
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2025-12-19 16:42:52 +0800

    Update 18.c

--- 18.c
+++ 18.c
@@ -7,18 +7,22 @@ int main(){
     char ext[20];
     char p = '.';
     int j= 0;
-    for(int i = len - 1; i > 0; i--){
+    for(int i = len -1; i >= 0; i--){
      if(str[i] == p) {
          for(int h = i; h < len; h++){
              ext[j] = str[h];
              j++;
          }
-        printf("%s", ext);
-        return 0;
+         break;
      }
-        
     }
-
-  // https://open.kattis.com/problems/nafnauki?editresubmit=18871372
+    ext[j] = '\0';
+       printf("%s", ext);
+    
+        return 0;
     
 }
+// https://open.kattis.com/problems/nafnauki?editresubmit=18874453
+// I need to put '\0' after I make a string
+// I cant compare using "." it will retiurn to the pointer
+// instead use p = '.' if I need to compare
