a4f451562164c15e020ef9e778fb8646a5ec8135
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2025-12-30 20:23:39 +0800

    Implement calculation for King Arthur problem

--- 23.c
+++ 23.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+int main(){
+    double pi = 3.14159;
+    double d, w, n;
+    scanf("%lf", &d);
+    scanf("%lf", &w);
+    scanf("%lf", &n);
+    
+    double p = 2.0 * pi * (d/2.0);
+    double k = w * n;
+    if(p >= k){
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+    
+    return 0;
+}
 
+https://open.kattis.com/problems/kingarthur?editresubmit=18900108
