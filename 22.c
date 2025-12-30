fbad6cde85cdf87bc882aefbb31a52f3081c5398
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2025-12-30 20:36:30 +0800

    Update 22.c

--- 22.c
+++ 22.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+int main(){
+    int n;
+    int q;
+    int sum = 0;
+    scanf("%d", &n);
+    int denominator = n;
+    for(int i = 0; i < n; i++){
+        scanf("%d", &q);
+        if(q>=0){
+            sum += q;
+        } else{
+            denominator -= 1;
+        }
+    }
+    double res = (double) sum/denominator;
+    printf("%lf", res);
+    return 0;
+}
 
+//https://open.kattis.com/problems/batterup?editresubmit=18900140
+// I learned that to get the double result of dividing int I need to put (double) before the formula
