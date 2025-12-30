1fb3cb3b852889f2c7840a282bc908aabe60c231
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2025-12-30 20:13:08 +0800

    Correct date check for Halloween and Christmas
    Fix string comparison and scanf usage for date check.

--- 24.c
+++ 24.c
@@ -1,1 +1,25 @@
+// https://open.kattis.com/problems/isithalloween?editresubmit=18900073
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+    
+    char month[10];
+    int day;
+    
+    scanf("%s %d", month, &day);
+    
+    if((strcmp(month, "OCT") == 0 && day == 31) ||
+    (strcmp(month, "DEC") == 0 && day == 25)
+    ){
+        printf("yup");
+    } else{
+        printf("nope");
+    }
+    return 0;
+}
+
+// I forgot that I cant use == to compare strings
+// I need to use the string.h function strcmp
+// I was reminded that I dont need to add ampersand when using scanf for string
 
