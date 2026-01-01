7b9861987ba386fd79e516271ef20181d259ad8b
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2026-01-01 14:02:06 +0800

    Correct result calculation in main function
    Fix calculation logic for result based on input values.

--- 28.c
+++ 28.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+int main(){
+    int s, r;
+    scanf("%d %d", &r, &s);
+    int res = (2*s) - r;
+    printf("%d", res);
+    return 0;
+}
 
+// https://open.kattis.com/problems/r2?editresubmit=18903649
+// I should be careful about the order of the input
