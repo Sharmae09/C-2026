4fa27c7b58c0d26095451ab4285ed09af82a216c
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2026-01-01 13:56:54 +0800

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int x, y;
+    scanf("%d %d", &x, &y);
+    int res =  (x * (y -1) +1);
+    printf("%d", res);
+    return 0;
+}
+
+// https://open.kattis.com/problems/faktor?editresubmit=18903647
+// i forgot & again
