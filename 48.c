63e3e426b4cc210ceec10f69b06f9a816df8f1cd
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2026-01-05 11:18:15 +0800

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    
+    char s[1000];
+    scanf("%s", s);
+    printf("Kvedja,\n %s", s);
+    return 0;
+    
+}
+// https://open.kattis.com/problems/kvedja?editresubmit=18914839
