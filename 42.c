fd5d602050f9b14070a131b9ad609ebd8bb4a4e0
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2026-01-05 10:58:54 +0800

    Add basic program to multiply three integers

---
+++ 42.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    
+    int x, y, z;
+    scanf("%d", &x);
+    scanf("%d", &y);
+    scanf("%d", &z);
+    printf("%d", x*z);
+    return 0;
+}
+
+// https://open.kattis.com/problems/parkingpandemonium?editresubmit=18914751
