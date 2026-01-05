7c7dc91d1e57abee18aeb52668b6e75c9c679930
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2026-01-05 11:23:18 +0800

    Add basic program to calculate and print result

---
+++ 49.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int x, y;
+    scanf("%d", &x);
+    scanf("%d", &y);
+    printf("%d", 2022 + (x/y));
+    return 0;
+}
+
+// https://open.kattis.com/problems/fifa?editresubmit=18914856
