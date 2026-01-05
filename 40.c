ab62ccd7d3fe0ee49e0f5872836b6b4f967d0573
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2026-01-05 10:51:08 +0800

    Create 40.c

---
+++ 40.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    
+    int x, y, z;
+    
+    scanf("%d", &x);
+    scanf("%d", &y);
+    scanf("%d", &z);
+     printf("%d",x*2 );
+    return 0;
+    
+}
+// https://open.kattis.com/problems/edays?editresubmit=18914722
+// Its one of those trick questions with redundant inputs
