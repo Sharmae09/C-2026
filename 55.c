6c1455b5987f76a380c2b3875df3bbab7d7094ec
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2026-01-06 11:16:42 +0800

    Add program to calculate z - x - y

---
+++ 55.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int x, y, z;
+    scanf("%d %d %d", &x, &y,&z);
+    
+    printf("%d", z-x-y);
+    return 0;
+}
+// https://open.kattis.com/problems/aleidibio?editresubmit=18917613
