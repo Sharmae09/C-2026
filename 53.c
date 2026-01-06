f48aab520cc9b80e529d9522f57c28de1e9d365f
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2026-01-06 11:14:54 +0800

    Correct calculation to use modulus operator
    Fix logic error in calculation and update output.

---
+++ 53.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    
+        int x, y;
+    scanf("%d ", &x);
+       scanf("%d ", &y);
+        // printf("%d", x-(y*2));
+        printf("%d", x%y);
+    return 0;
+}
+
+// I used the wrong logic because I did not read the problem first
+// https://open.kattis.com/problems/flatbokuveisla?editresubmit=18917632
