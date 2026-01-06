c63384e3bf6238d6a8d7b3ee8090d68d00862963
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2026-01-06 11:17:32 +0800

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int x;
+    scanf("%d ", &x);
+    printf("%d", x-1);
+    return 0;
+}
+// easy peasy lemon squeezy
+// https://open.kattis.com/problems/tolvunarfraedingartelja?editresubmit=18917621
