730dfef7eaba3532a05a5119c7b25bf0258ba92e
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2026-01-09 10:43:48 +0800

    Create 70.c

---
+++ 70.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int x, y;
+    scanf("%d %d", &x, &y);
+    printf("%d", x*y);
+    return 0;
+    
+}
+
+// https://open.kattis.com/problems/classphoto?editresubmit=18927568
