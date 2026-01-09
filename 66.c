50b6e40ad0a089d5fac025fca7bfaeb1c52cf656
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2026-01-09 09:57:23 +0800

    Create multiplication table program in C
    This program reads an integer and prints its multiplication table up to 12.

---
+++ 66.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    for(int i = 1; i <= 12; i++){
+        printf("%d\n", n*i);
+    }
+    
+    
+    return 0;
+    
+}
+
+// https://open.kattis.com/problems/counting?editresubmit=18927460
+// I feel do happy
