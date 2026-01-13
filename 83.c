3b90507fd73730b7bf7047dc3185b4e823407cca
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2026-01-13 23:19:52 +0800

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+  
+    if(n >= 13){
+        printf("%d", n+1);
+    }
+      else{
+        printf("%d", n);
+          
+      }
+    return 0;
+    
+}
+
+// https://open.kattis.com/problems/13floors?editresubmit=18948509
