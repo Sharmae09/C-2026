5deb127a4be549331be660450a5928aad515a297
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2026-01-23 22:05:28 +0800

    Create 91.c

---
+++ 91.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    if(n a% 2 == 0){
+        printf("second");
+    } else {
+        printf("first");
+    }
+    
+    return 0;
+}
