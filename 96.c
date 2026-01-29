19b3c5a2ed46050c0dd1ee41b157dff84a4e81b7
Sharmae_Dev <94461537+Sharmae09@users.noreply.github.com>
2026-01-29 21:22:04 +0800

    Add volume comparison for box dimensions
    The program calculates the volume of a box and compares it to a given capacity, printing appropriate messages based on the comparison.

--- 96.c
+++ 96.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+
+int main(){
+    int h, w, l, c;
+    
+    scanf("%d %d %d %d",  &h, &w, &l, &c);
+    int vol = h*w*l ; 
+    if(vol  == c){
+        printf("COZY");
+    }
+    else if(vol  < c){
+        printf("TOO TIGHT");
+    }
+    else if(vol  > c){
+        printf("SO MUCH SPACE");
+    }
+    
+    
+    return 0;
+}
+
+// https://open.kattis.com/problems/catinabox?editresubmit=19054356
